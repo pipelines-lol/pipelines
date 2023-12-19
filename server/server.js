c00fc817dfb3be4af3f6c28cdb88c3e5c78e34cc
ouckah <aidan.ouckama@gmail.com>
2023-12-18 21:10:15 -0500

    ✨ Routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const cors = require('cors')
 
 // route imports
 const authRoutes = require("./routes/auth")
-const pollRoutes = require('./routes/polls')
+const profileRoutes = require('./routes/profiles')
+const pipelineRoutes = require('./routes/pipelines')
 
 dotenv.config()
 const app = express()
@@ -24,6 +25,7 @@ app.use(express.json())
 
 // routes
 app.use('/api/user', authRoutes)
-app.use('/api/poll', pollRoutes)
+app.use('/api/profile', profileRoutes)
+app.use('/api/pipeline', pipelineRoutes)
 
 app.listen(PORT, () => console.log("Server is running."))
