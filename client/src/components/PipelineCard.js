4a866cc5a5c8885d6761c398e788b1eed7aee437
ouckah <aidan.ouckama@gmail.com>
2023-12-19 00:24:54 -0500

    ✨ Basic Profile Generation

---
+++ client/src/components/PipelineCard.js
@@ -0,0 +1,33 @@
+
+export const PipelineCard = ({ name, pipeline }) => {
+    return (
+        <div className='flex flex-row justify-center items-center bg-gray-200 p-12 rounded-2xl gap-10' key={pipeline._id}>
+            <div className="flex flex-col justify-center items-center gap-3">
+                <img className='w-36 h-36 rounded-full' src="avatar.png" />
+                <h1 className="text-black font-medium text-xl">{name}</h1>
+            </div>
+            <div className="flex flex-row gap-3">
+            {
+                pipeline.map((experience, i) => (
+                    <div className='flex flex-row justify-center items-center gap-3' key={experience._id}>
+                        <ExperienceCard experience={experience} />
+                        {
+                            (i != pipeline.length - 1) ? (<h1>--</h1>) : (<></>)
+                        }
+                    </div>
+                ))
+            }
+            </div>
+        </div>
+    )
+}
+
+const ExperienceCard = ({ experience }) => {
+    return (
+        <div className="flex flex-col justify-center items-center" key={experience._id}>
+            <h1 className="text-black font-semibold text-2xl">{experience.company}</h1>
+            <h1 className="text-black font-thin italic text-xl">{experience.title}</h1>
+            <h1 className="text-black opacity-60 font-light text-xl">{experience.date}</h1>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,64 @@
+import { useState, useEffect } from "react";
+
+import { PipelineCard } from "../components/PipelineCard";
+
 function Home() {
+
+    const [profiles, setProfiles] = useState([]);
+
+    const generateProfiles = async () => {
+        const size = 5;
+
+        const response = fetch(`http://localhost:4000/api/pipeline/random/${size}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json' // Specify the content type as JSON
+            },
+        })
+        .then((res) => {
+            if (!res.ok) {
+                // Check if the response has JSON content
+                if (res.headers.get('content-type')?.includes('application/json')) {
+                    return res.json().then((errorData) => {
+                        throw new Error(`${errorData.error}`);
+                    });
+                } else {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setProfiles([...data]);
+            console.log(data);
+
+            // localStorage.setItem('user', JSON.stringify(data));
+
+            // update context
+            // dispatch({ type: "LOGIN", payload: data });
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+    }
+
+    useEffect(() => {
+        generateProfiles();
+    }, [])
+
     return (
         <>
         
             <div className="flex flex-col justify-center items-center w-full h-full bg-gray-100">
                 <h1 className="text-black font-bold text-3xl p-8">Pipelines</h1>
+
+                <div>
+                {
+                    profiles.map((profile) => (
+                        <PipelineCard name={profile.name} pipeline={profile.pipeline} />
+                    ))
+                }
+                </div>
             </div>
         
         </>
