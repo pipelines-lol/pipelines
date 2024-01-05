e6229d669d8bdde12771c6303412c88c8bf08b5f
ouckah <aidan.ouckama@gmail.com>
2024-01-05 00:33:51 -0500

    🐛 Prod Bug

--- client/src/components/QuerySearchInput.js
+++ client/src/components/QuerySearchInput.js
@@ -46,7 +46,11 @@ export const QuerySearchInput = ({ handleSearch }) => {
                 <div className={`bg-white w-1/2 max-h-96 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
                     {hasResults && results.map((company) => (
                         <div className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100">
-                            <img className="w-10 h-10 rounded-lg" src={`logos/${company.logo}`} />
+                            <img 
+                                className="w-10 h-10 rounded-lg" 
+                                src={`logos/${company.logo}`} 
+                                alt={`logo_${company.name}`}
+                            />
                             <button
                                 key={company.id} // Add a unique key for each button
                                 className="w-full h-16 p-5 text-start"
