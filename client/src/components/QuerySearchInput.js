e11dd77fbfa2a51bae1cb3823972c62b272d158d
ouckah <aidan.ouckama@gmail.com>
2024-01-05 00:32:55 -0500

    ✨ Company Search on Home

--- client/src/components/QuerySearchInput.js
+++ client/src/components/QuerySearchInput.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { companies } from "../data/companyData"
 
-export const QuerySearchInput = () => {
+export const QuerySearchInput = ({ handleSearch }) => {
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
     let hasResults = results.length > 0;
@@ -21,34 +21,43 @@ export const QuerySearchInput = () => {
         }
     };
 
+    const handleCompanyButtonClick = async (company) => {
+        // reset text input
+        setQuery("");
+        setResults([]);
+
+        await handleSearch(company.name);
+    }
+
     return (
         <>
         
-        <form
-            className={`flex flex-col justify-center items-center w-full ${hasResults ? "relative" : ""}`}
-            onSubmit={(e) => e.preventDefault()} // Prevent form submission
-        >
-            <input
-                className={`w-1/2 h-20 z-10 bg-gray-200 rounded-2xl text-xl p-10`}
-                type="text"
-                value={query}
-                onChange={handleInputChange}
-                placeholder="Search companies..."
-            />
-            <div className={`bg-white w-1/2 max-h-96 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
-                {hasResults && results.map((company) => (
-                    <div className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100">
-                        <img className="w-10 h-10 rounded-lg" src={`logos/${company.logo}`} />
-                        <button
-                            key={company.id} // Add a unique key for each button
-                            className="w-full h-16 p-5 text-start"
-                        >
-                            <h1>{company.name}</h1>
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </form>
+            <form
+                className={`flex flex-col justify-center items-center w-full ${hasResults ? "relative" : ""}`}
+                onSubmit={(e) => e.preventDefault()} // Prevent form submission
+            >
+                <input
+                    className={`w-1/2 h-20 z-10 bg-gray-200 rounded-2xl text-xl p-10`}
+                    type="text"
+                    value={query}
+                    onChange={handleInputChange}
+                    placeholder="Search companies..."
+                />
+                <div className={`bg-white w-1/2 max-h-96 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
+                    {hasResults && results.map((company) => (
+                        <div className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100">
+                            <img className="w-10 h-10 rounded-lg" src={`logos/${company.logo}`} />
+                            <button
+                                key={company.id} // Add a unique key for each button
+                                className="w-full h-16 p-5 text-start"
+                                onClick={() => handleCompanyButtonClick(company)}
+                            >
+                                <h1>{company.name}</h1>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </form>
         
         </>
     )
