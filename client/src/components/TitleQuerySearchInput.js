a3e311f89b1e9e88425c566833c606d1e4d81ef2
ouckah <aidan.ouckama@gmail.com>
2024-01-07 01:17:11 -0500

    ✨ Title Query

--- client/src/components/TitleQuerySearchInput.js
+++ client/src/components/TitleQuerySearchInput.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { companies } from "../data/companyData"
+import { titles } from "../data/companyData"
 
-export const ExperienceQuerySearchInput = ({ value, handleSearch }) => {
+export const TitleQuerySearchInput = ({ value, handleSearch }) => {
     const [query, setQuery] = useState("");
     const [results, setResults] = useState([]);
     let hasResults = results.length > 0;
@@ -26,23 +26,23 @@ export const ExperienceQuerySearchInput = ({ value, handleSearch }) => {
 
         // make sure theres an input before querying
         if (inputValue.length > 0) {
-            const filteredCompanies = companies.filter((company) =>
-                company.name.toLowerCase().startsWith(inputValue.toLowerCase())
+            const filteredCompanies = titles.filter((title) =>
+                title.toLowerCase().startsWith(inputValue.toLowerCase())
             );
             setResults(filteredCompanies);
         } else {
             setResults([]);
-
-            await handleSearch("");
         }
+
+        await handleSearch(inputValue);
     };
 
-    const handleCompanyButtonClick = async (company) => {
+    const handleTitleButtonClick = async (title) => {
         // reset text input
-        setQuery(company.name);
+        setQuery(title);
         setResults([]);
 
-        await handleSearch(company.name);
+        await handleSearch(title);
     }
 
     return (
@@ -53,26 +53,21 @@ export const ExperienceQuerySearchInput = ({ value, handleSearch }) => {
                 onSubmit={(e) => e.preventDefault()} // Prevent form submission
             >
                 <input
-                    className={`px-4 py-2 z-40 bg-gray-100 rounded-2xl`}
+                    className={`px-4 py-2 z-20 bg-gray-100 rounded-2xl`}
                     type="text"
                     value={query}
                     onChange={handleInputChange}
                     placeholder="Google"
                 />
-                <div className={`bg-white w-full max-h-96 z-30 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
-                    {hasResults && results.map((company) => (
+                <div className={`bg-white w-full max-h-96 z-10 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
+                    {hasResults && results.map((title) => (
                         <div className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100">
-                            <img 
-                                className="w-10 h-10 rounded-lg" 
-                                src={`logos/${company.logo}`} 
-                                alt={`logo_${company.name}`}
-                            />
                             <button
-                                key={company.id} // Add a unique key for each button
+                                key={title} // Add a unique key for each button
                                 className="flex items-center w-full h-16 p-5 text-start"
-                                onClick={() => handleCompanyButtonClick(company)}
+                                onClick={() => handleTitleButtonClick(title)}
                             >
-                                <h1>{company.name}</h1>
+                                <h1>{title}</h1>
                             </button>
                         </div>
                     ))}
