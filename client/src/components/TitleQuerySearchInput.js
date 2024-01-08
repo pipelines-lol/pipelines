7b8c5e85b2f7ca975d4ccd0fc2a903c77194cc8e
ouckah <aidan.ouckama@gmail.com>
2024-01-08 17:37:07 -0500

    ✅ Add Keys

--- client/src/components/TitleQuerySearchInput.js
+++ client/src/components/TitleQuerySearchInput.js
@@ -61,7 +61,10 @@ export const TitleQuerySearchInput = ({ value, handleSearch }) => {
                 />
                 <div className={`bg-white w-full max-h-96 z-10 -translate-y-2 overflow-y-scroll shadow-md ${hasResults ? "absolute top-full" : ""}`}>
                     {hasResults && results.map((title) => (
-                        <div className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100">
+                        <div 
+                            key={`${title}_result`}
+                            className="flex flex-row justify-center items-center px-5 py-2 hover:bg-gray-100"
+                        >
                             <button
                                 key={title} // Add a unique key for each button
                                 className="flex items-center w-full h-16 p-5 text-start"
