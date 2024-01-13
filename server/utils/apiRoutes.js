2a6785b886aa6c5a6991e6c598e8b385d8a33695
ouckah <aidan.ouckama@gmail.com>
2024-01-13 01:08:09 -0500

    🔥 Backend Fix

--- server/utils/apiRoutes.js
+++ server/utils/apiRoutes.js
@@ -1,15 +1,17 @@
-const HOST =
-  process.env.NODE_ENV === "DEV"
-    ? "http://localhost:4000"
-    : process.env.NODE_ENV === "PROD"
-    ? "https://pipelines-backend.onrender.com"
-    : (console.error("Unknown mode:", process.env.MODE), null);
+const config = {
+  HOST:
+    process.env.NODE_ENV === "DEV"
+      ? "http://localhost:4000"
+      : process.env.NODE_ENV === "PROD"
+      ? "https://pipelines-backend.onrender.com"
+      : (console.error("Unknown mode:", process.env.MODE), null),
 
-const HOMEPAGE =
-  process.env.NODE_ENV === "DEV"
-    ? "http://localhost:3000"
-    : process.env.NODE_ENV === "PROD"
-    ? "https://pipelines.lol"
-    : (console.error("Unknown mode:", process.env.MODE), null);
+  HOMEPAGE:
+    process.env.NODE_ENV === "DEV"
+      ? "http://localhost:3000"
+      : process.env.NODE_ENV === "PROD"
+      ? "https://pipelines.lol"
+      : (console.error("Unknown mode:", process.env.MODE), null)
+};
 
-export { HOST, HOMEPAGE };
+module.exports = config;
