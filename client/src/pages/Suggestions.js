6191c07e69fda21e0f2021d1d06a8b3c544c6c15
Toye Sobayo <sobayotoye482@gmail.com>
2024-01-12 23:59:42 -0500

    added forms for connecting to backend

--- client/src/pages/Suggestions.js
+++ client/src/pages/Suggestions.js
@@ -1,47 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Suggestions = () => {
-    const [suggestionType, setSuggestionType] = useState('');
+  const [suggestionType, setSuggestionType] = useState("");
 
-    const handleDropdown = (event) => {
-        setSuggestionType(event.target.value);
-    };
+  const handleDropdown = (event) => {
+    setSuggestionType(event.target.value);
+  };
 
-    return (
-        <div className="flex justify-center items-center w-full h-[90vh] bg-gray-100">
-            <div className="flex flex-col justify-center items-center w-96 h-2/3 bg-white shadow-md py-10 gap-10">
-                <h1 className="text-black font-semibold text-2xl tracking-wide uppercase">Suggestions</h1>
-                
-                <div className="flex flex-col gap-3 w-full px-4">
-                    
-                    <select
-                        value={suggestionType}
-                        onChange={handleDropdown}
-                        className="px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none w-full" // Adjusted the class to match the input style
-                    >
-                        <option value="" disabled hidden> Suggestion Type</option>
-                        <option value="company">Company</option>
-                        <option value="website issues">Bugs/Issues</option>
-                        <option value="other">Other</option>
-                    </select>
+  return (
+    <div className="flex justify-center items-center w-full h-[90vh] bg-gray-100">
+      <div className="flex flex-col justify-center items-center w-96 h-2/3 bg-white shadow-md py-10 gap-10">
+        <h1 className="text-black font-semibold text-2xl tracking-wide uppercase">
+          Suggestions
+        </h1>
 
-                    <input className = "mt-10 mb-4 px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none w-full"
-                           placeholder="You guys smell"
-                    />
-                        
-                    
-                </div>
+        <div className="flex flex-col gap-3 w-full px-4">
+          <select
+            value={suggestionType}
+            onChange={handleDropdown}
+            className="mb-3 px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none w-full" // Adjusted the class to match the input style
+          >
+            <option value="" disabled hidden>
+              {" "}
+              Suggestion Type
+            </option>
+            <option value="company">Company</option>
+            <option value="website issues">Bugs/Issues</option>
+            <option value="other">Other</option>
+          </select>
 
-                
-                <button 
-                    className="bg-black px-12 py-2 rounded-full"
-                    onClick={() => {}}
-                >
-                    <h1 className="text-white">Submit Suggestion</h1>
-                </button>
-            </div>
+          <form action="/" method="POST">
+            <input
+              className="mt-7 mb-4 px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none w-full"
+              placeholder="You guys smell"
+            />
+          </form>
         </div>
-    );
+
+        <form action="/" method="POST">
+          <button
+            className="bg-black px-12 py-2 rounded-full"
+            onClick={() => {}}
+          >
+            <h1 className="text-white">Submit Suggestion</h1>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Suggestions;
