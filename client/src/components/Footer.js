727f264b2e07483267bc8047f3e969fba91da0a2
Toye Sobayo <sobayotoye482@gmail.com>
2024-01-12 19:46:16 -0500

    suggestion button working

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,9 @@
-const Footer = () => {
-    // const navigate = useNavigate();
+import { useNavigate } from "react-router-dom";
 
-    // const handleButton = () => {
-    //     navigate('/suggestions')
-    // };
+const Footer = () => {
+    const navigate = useNavigate();
+
+    
     
     return (
         <>
@@ -11,15 +11,12 @@ const Footer = () => {
             <div className="flex flex-row justify-between items-center w-full h-20 px-12 bg-pipelines-gray-500">
                 <h1 className="text-white font-light">@2024 pipelines.lol. All rights reserved.</h1>
                 <button 
-                    // onClick = {handleButton}
+                     onClick = {() => navigate('/suggestions')}
                     className = "text-white bg-pipelines-blue-500 hover:bg-pipelines-blue-600 px-4 py-2 rounded"
                      >
                         Have a suggestion?
                 </button>
             </div>
-
-            
-        
         </>
     )
 }
