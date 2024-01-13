573cf7308cd37fc02f673e910c73bcc73329abae
Toye Sobayo <sobayotoye482@gmail.com>
2024-01-12 20:15:38 -0500

    changed suggestion from button to link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
-    const navigate = useNavigate();
-
-    
     
     return (
         <>
         
             <div className="flex flex-row justify-between items-center w-full h-20 px-12 bg-pipelines-gray-500">
                 <h1 className="text-white font-light">@2024 pipelines.lol. All rights reserved.</h1>
-                <button 
-                     onClick = {() => navigate('/suggestions')}
+                <Link 
+                    to = "'/suggestions"
                     className = "text-white bg-pipelines-blue-500 hover:bg-pipelines-blue-600 px-4 py-2 rounded"
                      >
                         Have a suggestion?
-                </button>
+                </Link>
             </div>
         </>
     )
