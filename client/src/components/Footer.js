8871fb8497caa6b4fe74079fa3879e74f0707781
Toye Sobayo <sobayotoye482@gmail.com>
2024-01-12 19:02:58 -0500

    first commit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,24 @@
 const Footer = () => {
     // const navigate = useNavigate();
 
+    // const handleButton = () => {
+    //     navigate('/suggestions')
+    // };
+    
     return (
         <>
         
             <div className="flex flex-row justify-between items-center w-full h-20 px-12 bg-pipelines-gray-500">
                 <h1 className="text-white font-light">@2024 pipelines.lol. All rights reserved.</h1>
+                <button 
+                    // onClick = {handleButton}
+                    className = "text-white bg-pipelines-blue-500 hover:bg-pipelines-blue-600 px-4 py-2 rounded"
+                     >
+                        Have a suggestion?
+                </button>
             </div>
+
+            
         
         </>
     )
