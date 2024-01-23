78e95efb80d36c75d536a3a8a2ca0d2df876acd0
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-01-23 10:22:02 -0500

    Create and Test Get procedure

--- server/app.test.js
+++ server/app.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const router = require('./routes/companies')
 const Company = require('./models/companyModel')
 const mongoose = require('mongoose')
+const server = require('./index')
 const app = require('./index')
 
 
@@ -20,6 +21,27 @@ describe('POST calls', () => {
 })
 
 describe('GET calls', () => {
+
+    test('get-company', async () => {
+        const name = 'TestCompany';
+        const response = await api.get(`/get/${name}`);
+    
+        // Check if the response status is okay (status code 200-299)
+        expect(response.ok).toBeTruthy();
+    
+        // Parse the JSON in the response
+        const jsonData = await response.body;
+    
+        console.log("Get response", response.status);
+        console.log("Response JSON: ", jsonData);
+    
+        // Now you can assert or perform further checks on the extracted JSON data
+        expect(jsonData).toHaveProperty('name');
+        expect(jsonData.name).toBe('TestCompany');
+        // Add more assertions based on your JSON structure
+    });
+    
+
     test('delete-company', async () => {
         const name = 'TestCompany';
         const response = await api.delete(`/delete/${name}`)
@@ -32,7 +54,6 @@ describe('GET calls', () => {
 
 afterAll(done => {
     // Closing the DB connection allows Jest to exit successfully.
-    app.listen()
     mongoose.connection.close()
     done()
 })
