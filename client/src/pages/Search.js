1af791aa4c11262c3f29bbe5edf66ec7f77f7bdf
Thomas <nyumat18@gmail.com>
2024-01-24 22:29:19 -0800

    🚀 refactor: applied design across pages

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,82 +1,100 @@
-import { useState } from "react";
+import { useState } from 'react'
 
-import { PipelineCard } from "../components/PipelineCard";
-import { HOST } from "../util/apiRoutes";
-import { QuerySearchInput } from "../components/QuerySearchInput";
-import Loading from "./Loading";
+import { PipelineCard } from '../components/PipelineCard'
+import { QuerySearchInput } from '../components/QuerySearchInput'
+import { HOST } from '../util/apiRoutes'
+import Loading from './Loading'
 
 function Search() {
-  const [profiles, setProfiles] = useState([]);
+    const [profiles, setProfiles] = useState([])
 
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
 
-  const handleSearch = async (query) => {
-    // loading state to load query
-    setLoading(true);
+    const handleSearch = async (query) => {
+        // loading state to load query
+        setLoading(true)
 
-    fetch(`${HOST}/api/pipeline/search/${query}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json", // Specify the content type as JSON
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          // Check if the response has JSON content
-          if (res.headers.get("content-type")?.includes("application/json")) {
-            return res.json().then((errorData) => {
-              throw new Error(`${errorData.error}`);
-            });
-          } else {
-            throw new Error(`HTTP error! Status: ${res.status}`);
-          }
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setProfiles([...data]);
+        fetch(`${HOST}/api/pipeline/search/${query}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json', // Specify the content type as JSON
+            },
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    // Check if the response has JSON content
+                    if (
+                        res.headers
+                            .get('content-type')
+                            ?.includes('application/json')
+                    ) {
+                        return res.json().then((errorData) => {
+                            throw new Error(`${errorData.error}`)
+                        })
+                    } else {
+                        throw new Error(`HTTP error! Status: ${res.status}`)
+                    }
+                }
+                return res.json()
+            })
+            .then((data) => {
+                setProfiles([...data])
 
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  };
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.error(error.message)
+            })
+    }
 
-  if (loading) {
-    return <Loading />;
-  }
+    if (loading) {
+        return <Loading />
+    }
 
-  return (
-    <>
-      <div className="flex flex-col justify-center items-center w-full h-full min-h-[100vh] bg-white gap-12 pt-15">
-        <div className="flex flex-col justify-center items-center text-center w-full h-[50vh] bg-pink-100 gap-5">
-          <div className="flex flex-col justify-center items-center text-center w-full gap-3">
-            <h1 className="text-pipelines-gray-500 font-bold text-4xl">
-              Find Your <span className="text-pink-700">Pipeline</span>
-            </h1>
-            <p className="text-pipelines-gray-500 font-light text-xl">
-              See where you were. Find where you are. <br /> Search where you
-              can be.
-            </p>
-          </div>
-          <QuerySearchInput handleSearch={handleSearch} />
-        </div>
-        <div className="grid md:grid-cols-4 grid-cols-2 md:gap-4 sm:gap-2 gap-1 pb-12">
-          {profiles.map((profile) => (
-            <PipelineCard
-              key={`pipeline_${profile._id}`}
-              profileId={profile._id}
-              name={profile.firstName + " " + profile.lastName}
-              pfp={profile.pfp}
-              anonymous={profile.anonymous}
-              pipeline={profile.pipeline}
-            />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex h-[89vh] w-full flex-col items-center justify-center gap-12 bg-black/20 pt-24">
+                <div
+                    className="flex w-full flex-col items-center justify-center gap-5 bg-pipeline-blue-200/20 text-center"
+                    style={{
+                        backgroundImage: 'url("hero.png")',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        height: '50dvh',
+                        borderBottom: '1px solid rgba(2, 101, 172, 0.2)',
+                        borderTop: '1px solid rgba(2, 101, 172, 0.3)',
+                    }}
+                >
+                    <div className="flex w-full flex-col items-center justify-center gap-3 text-center">
+                        <h1 className="text-4xl font-light text-pipelines-gray-100 md:text-6xl">
+                            Find Your{' '}
+                            <span className="text-pipeline-blue-200">
+                                Pipeline
+                            </span>
+                        </h1>
+                        <p className="text-xl font-light text-pipelines-gray-100/80">
+                            See where you were. Find where you are. <br />{' '}
+                            Search where you can be.
+                        </p>
+                    </div>
+                    <QuerySearchInput handleSearch={handleSearch} />
+                </div>
+                <div className="grid grid-cols-2 gap-1 pb-12 sm:gap-2 md:grid-cols-4 md:gap-4">
+                    {profiles.map((profile) => (
+                        <PipelineCard
+                            key={`pipeline_${profile._id}`}
+                            profileId={profile._id}
+                            name={profile.firstName + ' ' + profile.lastName}
+                            pfp={profile.pfp}
+                            anonymous={profile.anonymous}
+                            pipeline={profile.pipeline}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Search;
+export default Search
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,101 +1,140 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuthContext } from "../hooks/useAuthContext";
-import { HOST } from "../util/apiRoutes";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import { HOST } from '../util/apiRoutes'
 
 function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const [errorMessage, setErrorMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState('')
 
-  const { dispatch } = useAuthContext();
+    const { dispatch } = useAuthContext()
 
-  const navigate = useNavigate();
+    const navigate = useNavigate()
 
-  const signUp = (email, password) => {
-    const _user = { email, password };
+    const signUp = (email, password) => {
+        const _user = { email, password }
 
-    // validation
-    if (email.includes(" ") || password.includes(" ")) {
-      setErrorMessage("Invalid username or password.");
-      return;
-    }
-
-    fetch(`${HOST}/api/user/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json", // Specify the content type as JSON
-      },
-      body: JSON.stringify(_user),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          // Check if the response has JSON content
-          if (res.headers.get("content-type")?.includes("application/json")) {
-            return res.json().then((errorData) => {
-              throw new Error(`${errorData.error}`);
-            });
-          } else {
-            throw new Error(`HTTP error! Status: ${res.status}`);
-          }
+        // validation
+        if (email.includes(' ') || password.includes(' ')) {
+            setErrorMessage('Invalid username or password.')
+            return
         }
 
-        return res.json();
-      })
-      .then((data) => {
-        localStorage.setItem("user", JSON.stringify(data));
+        fetch(`${HOST}/api/user/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', // Specify the content type as JSON
+            },
+            body: JSON.stringify(_user),
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    // Check if the response has JSON content
+                    if (
+                        res.headers
+                            .get('content-type')
+                            ?.includes('application/json')
+                    ) {
+                        return res.json().then((errorData) => {
+                            throw new Error(`${errorData.error}`)
+                        })
+                    } else {
+                        throw new Error(`HTTP error! Status: ${res.status}`)
+                    }
+                }
 
-        // update AuthContext
-        dispatch({ type: "LOGIN", payload: data });
+                return res.json()
+            })
+            .then((data) => {
+                localStorage.setItem('user', JSON.stringify(data))
 
-        // redirect to home
-        navigate("/");
-      })
-      .catch((error) => {
-        setErrorMessage(error.message);
-      });
-  };
+                // update AuthContext
+                dispatch({ type: 'LOGIN', payload: data })
 
-  return (
-    <>
-      <div className="flex justify-center items-center w-full h-[90vh] bg-gray-100">
-        <div className="flex flex-col justify-center items-center w-96 h-2/3 bg-white shadow-md py-10 gap-10">
-          <h1 className="text-black font-semibold text-2xl tracking-wide uppercase">
-            Signup
-          </h1>
+                // redirect to home
+                navigate('/')
+            })
+            .catch((error) => {
+                setErrorMessage(error.message)
+            })
+    }
 
-          <div className="flex flex-col gap-3">
-            <label className="text-medium">Email</label>
-            <input
-              className="px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none"
-              placeholder="someone@gmail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+    return (
+        <>
+            <div className="flex h-[90vh] w-full items-center justify-center">
+                <div
+                    className="flex h-full w-full flex-col items-center justify-center gap-5 bg-pipeline-blue-200/20 text-center"
+                    style={{
+                        backgroundImage: 'url("hero.png")',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                        height: '70dvh',
+                        borderBottom: '1px solid rgba(2, 101, 172, 0.2)',
+                        borderTop: '1px solid rgba(2, 101, 172, 0.3)',
+                    }}
+                >
+                    <div className="mb-8 flex w-full flex-col items-center justify-center gap-3 text-center">
+                        <h1 className="text-3xl font-light text-pipelines-gray-100 md:text-4xl">
+                            Welcome to{' '}
+                            <span className="text-pipeline-blue-200">
+                                Pipelines.lol
+                            </span>
+                        </h1>
+                    </div>
+                    <div className="flex h-2/3 w-96 flex-col items-center justify-center gap-4 rounded-lg border border-pipeline-blue-200/20 bg-white/10 bg-opacity-10  py-24 shadow-md backdrop-blur-xl backdrop-filter">
+                        <h1 className="pb-2 text-2xl font-semibold uppercase tracking-wide text-pipelines-gray-100">
+                            Sign Up
+                        </h1>
 
-            <label className="text-medium">Password</label>
-            <input
-              className="px-4 py-2 text-gray-800 bg-gray-100 rounded-full outline-none"
-              type="password"
-              placeholder="••••••••••"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+                        <div className="flex flex-col gap-3">
+                            <label className="text-light text-pipelines-gray-100">
+                                Email
+                            </label>
+                            <input
+                                className={
+                                    'z-20 w-full rounded-2xl bg-gray-100/60 px-4 py-2 text-black outline-none placeholder:text-gray-950/30'
+                                }
+                                placeholder="someone@gmail.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
 
-          {errorMessage && <h1 className="text-red-400">{errorMessage}</h1>}
+                            <label className="text-light text-pipelines-gray-100">
+                                Password
+                            </label>
+                            <input
+                                className={
+                                    'z-20 w-full rounded-2xl bg-gray-100/60 px-4 py-2 text-black outline-none placeholder:text-gray-950/30'
+                                }
+                                type="password"
+                                placeholder="••••••••••"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
 
-          <button
-            className="bg-black px-12 py-2 rounded-full"
-            onClick={() => signUp(email, password)}
-          >
-            <h1 className="text-white">Sign Up</h1>
-          </button>
-        </div>
-      </div>
-    </>
-  );
+                        {errorMessage && (
+                            <h1 className="text-red-400">{errorMessage}</h1>
+                        )}
+
+                        <div className="flex flex-col pt-2 hover:cursor-pointer">
+                            <a
+                                onClick={() => signUp(email, password)}
+                                className="group relative z-0 mt-6 inline-flex items-center justify-center overflow-hidden rounded-lg bg-pipelines-gray-100 px-10 py-2 font-light tracking-wide text-black/80"
+                            >
+                                <span className="absolute h-0 w-0 rounded-full bg-pipeline-blue-200 transition-all duration-500 ease-out group-hover:h-56 group-hover:w-56"></span>
+                                <span className="absolute inset-0 -mt-1 h-full w-full rounded-lg bg-gradient-to-b from-transparent via-transparent to-gray-200 opacity-30"></span>
+                                <span className="relative">Join Pipelines</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Signup;
+export default Signup
