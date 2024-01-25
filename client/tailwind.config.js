9008df921679d2e39359692315ffef8859604684
Thomas <nyumat18@gmail.com>
2024-01-24 22:30:49 -0800

    ⚡️ config/refacotor: DRY routes and design system config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "pipelines-gray-500": "#444444",
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'pipelines-gray-100': '#F7F7F7',
+                'pipelines-gray-500': '#444444',
+                'pipeline-blue-200': '#0265AC',
+            },
+            animation: {
+                'infinite-scroll': 'infinite-scroll 265s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-180%)' },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
