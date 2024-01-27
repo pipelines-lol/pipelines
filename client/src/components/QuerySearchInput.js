3c066165c3071cdbb65fc988a6960108de68f5fa
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-01-27 01:04:39 -0500

    fixed some design bugs

--- client/src/components/QuerySearchInput.js
+++ client/src/components/QuerySearchInput.js
@@ -37,7 +37,7 @@ export const QuerySearchInput = ({ handleSearch }) => {
             >
                 <input
                     className={
-                        'z-20 h-20 w-4/6 rounded-2xl border-2 border-white/5 bg-pipeline-blue-200/10 p-10 text-xl focus:outline-none focus:ring-2 focus:ring-pipeline-blue-200 focus:ring-opacity-50 md:w-1/2'
+                        'z-20 h-20 w-4/6 rounded-2xl border-2 border-white/5 bg-pipeline-blue-200/10 p-10 text-xl text-white focus:outline-none focus:ring-2 focus:ring-pipeline-blue-200 focus:ring-opacity-50 md:w-1/2'
                     }
                     type="text"
                     value={query}
@@ -45,13 +45,13 @@ export const QuerySearchInput = ({ handleSearch }) => {
                     placeholder="Search companies..."
                 />
                 <div
-                    className={`z-10 max-h-96 w-1/2 -translate-y-2 overflow-y-scroll bg-white shadow-md ${hasResults ? 'absolute top-full' : ''}`}
+                    className={`z-10 max-h-96 w-1/2 -translate-y-2 overflow-y-scroll bg-pipeline-blue-200/10 shadow-md ${hasResults ? 'absolute top-full' : ''}`}
                 >
                     {hasResults &&
                         results.map((company) => (
                             <div
                                 key={`${company.name}_result`}
-                                className="flex flex-row items-center justify-center px-5 py-2 hover:bg-gray-100"
+                                className="flex flex-row items-center justify-center px-5 py-2 hover:bg-pipeline-blue-200"
                             >
                                 <img
                                     className="h-10 w-10 rounded-lg object-contain"
@@ -60,7 +60,7 @@ export const QuerySearchInput = ({ handleSearch }) => {
                                 />
                                 <button
                                     key={company.id} // Add a unique key for each button
-                                    className="h-16 w-full p-5 text-start"
+                                    className="h-16 w-full p-5 text-start text-white"
                                     onClick={() =>
                                         handleCompanyButtonClick(company)
                                     }
