70b8cff8139d158ba8b5bcb098e68473c82c3c17
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-03-24 00:53:14 -0400

    fix prettier issues

--- client/src/components/PipelineDisplay.js
+++ client/src/components/PipelineDisplay.js
@@ -122,14 +122,12 @@ export const ExperienceCard = ({ experience }) => {
                     className="h-24 w-24 rounded-md object-contain"
                     src={experience.logo}
                     alt={`${experience.displayName}_logo`}
-
                 />
                 <div className="absolute left-2 top-5 h-24 w-24 animate-blob rounded-full bg-pipelines-gray-100/20 opacity-70 mix-blend-multiply blur-xl filter" />
             </div>
             <div className="flex flex-col items-center justify-center">
                 <h1 className="text-2xl font-semibold text-pipelines-gray-100">
                     {experience.displayName}
-
                 </h1>
                 <h1 className="text-x font-thin text-pipelines-gray-100">
                     {experience.title}
