df285d93bc625952e2052bcb091de5c6de0e10eb
ouckah <aidan.ouckama@gmail.com>
2024-03-23 22:26:12 -0400

    ✨ Company Backend Tweaks

--- server/models/companyModel.js
+++ server/models/companyModel.js
@@ -6,7 +6,12 @@ const companySchema = new Schema({
     type: String,
     required: true,
   },
-  displayName: String,
+  displayName: {
+    type: String,
+    required: true,
+  },
+  logo: String,
+  description: String,
   rating: Number,
   prevCompanies: {
     type: Map,
