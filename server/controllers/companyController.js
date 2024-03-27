4b140ffdad77a066c4f9e39ef307a5a4be6b7d98
ouckah <aidan.ouckama@gmail.com>
2024-03-27 19:40:30 -0400

    ✨ Editable Fields

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -88,6 +88,9 @@ const updateCompany = async (req, res) => {
   //update company on user registration
   const name = req.params.name;
   const {
+    displayName,
+    logo,
+    description,
     rating,
     prevCompanies,
     postCompanies,
@@ -106,6 +109,7 @@ const updateCompany = async (req, res) => {
   } = req.body;
   const lowercaseCompanyName = name.toLowerCase();
 
+  //! THERE HAS TO BE A WAY TO SIMPLIFY THIS LOGIC HOLY FUUUUUUUUUCCCKKK
   try {
     if (Employees && Employees.length > 0) {
       for (const employee of Employees) {
@@ -208,7 +212,7 @@ const updateCompany = async (req, res) => {
     }
 
     //Increment list of postCompanies
-    if (postCompanies && postCompanies.lenth > 0) {
+    if (postCompanies && postCompanies.length > 0) {
       for (const companyName of postCompanies) {
         const updateData = {
           $inc: {},
@@ -318,6 +322,32 @@ const updateCompany = async (req, res) => {
           return;
         }
       }
+    }
+
+    // Build the update object dynamically
+    let updateFields = {};
+    if (displayName !== null && displayName !== undefined)
+      updateFields.displayName = displayName;
+    if (logo !== null && logo !== undefined) updateFields.logo = logo;
+    if (description !== null && description !== undefined)
+      updateFields.description = description;
+
+    console.log(updateFields);
+
+    try {
+      const lowercaseCompanyName = name.toLowerCase();
+      const updatedCompany = await Company.findOneAndUpdate(
+        { name: lowercaseCompanyName },
+        { $set: updateFields },
+        { new: true }
+      );
+
+      if (!updatedCompany) {
+        res.status(404).send("Company not found");
+      }
+    } catch (error) {
+      console.error("Error updating company:", error);
+      res.status(500).send("Error updating company");
     }
 
     res.status(200).json({ message: "success" });
