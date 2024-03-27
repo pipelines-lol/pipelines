2fbdfe60eb0bcb5fef1199aa85d2c4eb084d3a79
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-03-26 22:28:10 -0400

    Revert "🐛 API fix + Limit query"

--- server/controllers/schoolController.js
+++ server/controllers/schoolController.js
@@ -1,30 +1,20 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const searchSchools = async (req, res) => {
-  const query = req.query.name;
-  const limit = parseInt(req.query.limit, 10) || 10;
-
-  try {
-    const response = await fetch(
-      `http://universities.hipolabs.com/search?name=${query}`
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    const query = req.query.name;
+    try {
+        const response = await fetch(`http://universities.hipolabs.com/search?name=${query}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        res.json(data); // Send the data back to the client over HTTPS
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
     }
-    let data = await response.json();
-
-    // if there's a limit, trim the data array to the specified length.
-    if (limit > 0) {
-      data = data.slice(0, limit);
-    }
-
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
-  }
-};
+}
 
 module.exports = {
-  searchSchools,
-};
+    searchSchools
+}
