12e36ea4547e185d5de407859c4cc53480825a23
ouckah <aidan.ouckama@gmail.com>
2024-03-26 20:29:25 -0400

    🐛 quick backend fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,101 +1,114 @@
-const User = require('../models/userModel')
-const Profile = require('../models/profileModel')
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
 
-const jwt = require('jsonwebtoken')
-const axios = require('axios')
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
 
-const { HOMEPAGE } = require('../utils/apiRoutes');
+const { HOMEPAGE } = require("../utils/apiRoutes");
 
 const createToken = (_id) => {
-    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' })
-}
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
-    const { email } = req.body
+  const { email } = req.body;
 
+  try {
+    const user = await User.login(email);
+
+    let profile;
+
+    // check to see if a profile has been made
     try {
-        const user = await User.login(email)
-
-        let profile 
-        
-        // check to see if a profile has been made
-        try {
-            profile = await Profile.getByUserId(user._id)
-        } 
-        
-        // no profile created yet
-        catch (error) {
-            // create new profile
-            profile = await Profile.create({ 
-                userId: user._id, 
-                name: '',
-                username: '',
-                linkedin: '',
-                position: '',
-                pfp: '',
-                location: '',
-                anonymous: false,
-                pipeline: [],
-                created: false
-            })
-
-            // update profileId on user
-            await User.updateOne({ _id: user._id }, { profileId: profile._id })
-        }
-
-        // create token
-        const token = createToken(user._id)
-
-        res.status(200).json({ email, _id: user._id, profileId: profile._id, profileCreated: profile.created, username: profile.username, token })
+      profile = await Profile.getByUserId(user._id);
     } catch (error) {
-        res.status(400).json({ error: error.message })
+      // no profile created yet
+      // create new profile
+      profile = await Profile.create({
+        userId: user._id,
+        firstName: "",
+        lastName: "",
+        username: "",
+        linkedin: "",
+        pfp: "",
+        position: "",
+        location: "",
+        anonymous: false,
+        school: "",
+        pipeline: [],
+        offers: [],
+        created: false,
+      });
+
+      // update profileId on user
+      await User.updateOne({ _id: user._id }, { profileId: profile._id });
     }
-}
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({
+      email,
+      _id: user._id,
+      profileId: profile._id,
+      profileCreated: profile.created,
+      username: profile.username,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const getLinkedinInfoWithCode = async (req, res) => {
-    const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID
-    const CLIENT_SECRET = process.env.LINKEDIN_CLIENT_SECRET
-    const REDIRECT_URI = HOMEPAGE
+  const CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
+  const CLIENT_SECRET = process.env.LINKEDIN_CLIENT_SECRET;
+  const REDIRECT_URI = HOMEPAGE;
 
-    try {
-        const code = req.headers.auth_code
-        if (!code) throw new Error('No code provided')
-        console.log("Code gotten succesfully");
+  try {
+    const code = req.headers.auth_code;
+    if (!code) throw new Error("No code provided");
+    console.log("Code gotten succesfully");
 
-        // This request gets access_token
-        let accessTokenResponse = await axios.get(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}`)
+    // This request gets access_token
+    let accessTokenResponse = await axios.get(
+      `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}`
+    );
 
+    // This request gets user info from access_token (given in the headers of the request)
+    let userInfoResponse = await axios.get(
+      "https://api.linkedin.com/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessTokenResponse.data.access_token}`,
+        },
+      }
+    );
 
-        // This request gets user info from access_token (given in the headers of the request)
-        let userInfoResponse = await axios.get('https://api.linkedin.com/v2/userinfo', {
-            headers: {
-                'Authorization': `Bearer ${accessTokenResponse.data.access_token}`
-            }
-        })
+    // This request gets profile info from access_token (given in the headers of the request)
+    let userBasicProfileResponse = await axios.get(
+      "https://api.linkedin.com/v2/me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessTokenResponse.data.access_token}`,
+        },
+      }
+    );
 
-        // This request gets profile info from access_token (given in the headers of the request)
-        let userBasicProfileResponse = await axios.get('https://api.linkedin.com/v2/me', {
-            headers: {
-                'Authorization': `Bearer ${accessTokenResponse.data.access_token}`
-            }
-        })
-        
-        // Get the "vanityName" value from userBasicProfileResponse
-        const vanity_name = userBasicProfileResponse.data.vanityName;
+    // Get the "vanityName" value from userBasicProfileResponse
+    const vanity_name = userBasicProfileResponse.data.vanityName;
 
-        // Attach the "vanityName" value to the userInfoResponse object as "vanity_name"
-        userInfoResponse.data.vanity_name = vanity_name;
-        console.log("user info ", userInfoResponse.data)
-        return res.status(200).json(userInfoResponse.data)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json({ error: error.message })
-
-    }
-}
+    // Attach the "vanityName" value to the userInfoResponse object as "vanity_name"
+    userInfoResponse.data.vanity_name = vanity_name;
+    console.log("user info ", userInfoResponse.data);
+    return res.status(200).json(userInfoResponse.data);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    loginUser,
-    getLinkedinInfoWithCode
-}
+  loginUser,
+  getLinkedinInfoWithCode,
+};
