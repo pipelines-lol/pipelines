0692d82d0fd34024aa69b263895d9152324f7f37
ouckah <aidan.ouckama@gmail.com>
2024-03-29 16:47:06 -0400

    🚑 Add Token Import

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const pfpRoutes = require("./routes/pfps");
 const imageModerationRoutes = require("./routes/imageModeration");
 const emailRoutes = require("./routes/emails");
 const earlyAccessRoutes = require("./routes/earlyAccess");
+const tokenRoutes = require("./routes/token");
 
 dotenv.config();
 
