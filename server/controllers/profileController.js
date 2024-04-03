ccb00bede6d80db77de1bd96392a245e82ff14f5
ouckah <aidan.ouckama@gmail.com>
2024-04-03 14:20:54 -0400

    🔥 More Logs

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -77,8 +77,6 @@ const deleteProfile = async (req, res) => {
 const updateProfile = async (req, res) => {
   const { id } = req.params;
 
-  console.log("Profile Controller Id:", id);
-
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such Profile." });
   }
