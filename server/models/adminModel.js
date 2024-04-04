240d30b9ec68e1e14705aebe8ddd0923feb7fdcf
ouckah <aidan.ouckama@gmail.com>
2024-04-04 01:22:05 -0400

    background work to add admins

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,25 +19,53 @@ const adminSchema = new Schema(
   { timestamps: true }
 );
 
+adminSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error(`All fields must be filled.`);
+  }
+  if (!validator.isEmail(email)) {
+    throw Error(`Email is invalid.`);
+  }
+
+  // Enable when strong passwords are needed.
+  // if (!validator.isStrongPassword(password)) {
+  //   throw Error(`Password is not strong enough.`)
+  // }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw Error(`Email already in use.`);
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const admin = await this.create({ email, password: hash });
+
+  return admin;
+};
+
 adminSchema.statics.login = async function (email, password) {
   // validation
   if (!email) {
     throw new Error("Email is required.");
   }
 
-  let user = await this.findOne({ email });
+  let admin = await this.findOne({ email });
 
-  if (!user) {
+  if (!admin) {
     throw new Error("Invalid email.");
   }
 
-  const match = await bcrypt.compare(password, user.password);
+  const match = await bcrypt.compare(password, admin.password);
 
   if (!match) {
-    throw Error(`Incorrect password.`);
+    throw new Error(`Incorrect password.`);
   }
 
-  return user;
+  return admin;
 };
 
 module.exports = mongoose.model("Admin", adminSchema);
